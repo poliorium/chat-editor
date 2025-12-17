e93f86809b06e34d826752a68775f7461c7abaf4
poliorium <206397731+poliorium@users.noreply.github.com>
2025-12-17 18:38:59 -0500

    i fix it aaaaaaaaaaaaaaaaaaaaaaaaaaaa

--- edit chat.lua
+++ edit chat.lua
@@ -1,17 +1,9 @@
---poliorium 4/6/25
-local Players = game:GetService("Players")
-local TextChatService = game:GetService("TextChatService")
-local localPlayer = Players.LocalPlayer
-TextChatService.OnIncomingMessage = function(message)
-    if message.TextSource and message.TextSource.UserId == localPlayer.UserId then
-        local messageId = message.MessageId  
-        wait(2)
-        replicatesignal(game.TextChatService.ClientToServerMessageReplicateSignalV2,
-            "edited message",
-            "b",
-            messageId, 
-            TextChatService.TextChannels.RBXGeneral[localPlayer.Name],  
-            TextChatService.TextChannels.RBXGeneral
-        )
+local lp = game.Players.LocalPlayer
+game.TextChatService.OnIncomingMessage = function(message)
+    if message.TextSource and message.TextSource.UserId == lp.UserId and message.Status == Enum.TextChatMessageStatus.Success then
+        local message_id = message.MessageId  
+        game.TextChatService.OnIncomingMessage = nil
+        task.wait(2)
+        replicatesignal(game.TextChatService.ClientToServerMessageReplicateSignalV2,"edited message","b",message_id, game.TextChatService.TextChannels.RBXGeneral[lp.Name],game.TextChatService.TextChannels.RBXGeneral)
     end
 end
